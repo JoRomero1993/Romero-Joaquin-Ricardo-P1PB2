e6a6a19cfc9aa5ef2bfaab8e35729c20bc48783b
JoRomero1993 <82046186+JoRomero1993@users.noreply.github.com>
2021-06-01 20:57:41 -0300

    vamos 9

--- src/ar/unlam/edu/parcialpb1/Biblioteca.java
+++ src/ar/unlam/edu/parcialpb1/Biblioteca.java
@@ -6,8 +6,46 @@ public class Biblioteca {
 	private ArrayList<Historia> Historia = new ArrayList<Historia>(); 
 	private ArrayList<Geografia> Geografia = new ArrayList<Geografia>();
 	private ArrayList<Matematicas> Matematicas = new ArrayList<Matematicas>();
-	public void set(Historia historia1, Geografia geografia1, Matematicas matematicas1) {
-		// TODO Auto-generated method stub
+	
+	public Biblioteca(ArrayList<ar.unlam.edu.parcialpb1.Historia> historia,
+			ArrayList<ar.unlam.edu.parcialpb1.Geografia> geografia,
+			ArrayList<ar.unlam.edu.parcialpb1.Matematicas> matematicas) {
+		super();
+		Historia = historia;
+		Geografia = geografia;
+		Matematicas = matematicas;
+	}
+
+	public ArrayList<Historia> getHistoria() {
+		return Historia;
+	}
+
+	public void setHistoria(ArrayList<Historia> historia) {
+		Historia = historia;
+	}
+
+	public ArrayList<Geografia> getGeografia() {
+		return Geografia;
+	}
+
+	public void setGeografia(ArrayList<Geografia> geografia) {
+		Geografia = geografia;
+	}
+
+	public ArrayList<Matematicas> getMatematicas() {
+		return Matematicas;
+	}
+
+	public void setMatematicas(ArrayList<Matematicas> matematicas) {
+		Matematicas = matematicas;
+	}
+	
+
+	
+	public void setCambiarVariosLibros(ArrayList<ar.unlam.edu.parcialpb1.Historia> historia1, ArrayList<ar.unlam.edu.parcialpb1.Geografia> geografia1, ArrayList<ar.unlam.edu.parcialpb1.Matematicas> matematicas1) {
+		this.Historia=historia1;
+		this.Geografia=geografia1;
+		this.Matematicas=matematicas1;
 		
 	}
 
