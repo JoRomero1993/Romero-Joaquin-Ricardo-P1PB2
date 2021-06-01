0d27872fa2045d6ff8c8b20d26e924436986eb0b
JoRomero1993 <82046186+JoRomero1993@users.noreply.github.com>
2021-06-01 20:49:44 -0300

    vamos 8

--- src/ar/unlam/edu/parcialpb1/Biblioteca.java
+++ src/ar/unlam/edu/parcialpb1/Biblioteca.java
@@ -1,5 +1,14 @@
 package ar.unlam.edu.parcialpb1;
 
+import java.util.ArrayList;
+
 public class Biblioteca {
+	private ArrayList<Historia> Historia = new ArrayList<Historia>(); 
+	private ArrayList<Geografia> Geografia = new ArrayList<Geografia>();
+	private ArrayList<Matematicas> Matematicas = new ArrayList<Matematicas>();
+	public void set(Historia historia1, Geografia geografia1, Matematicas matematicas1) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
