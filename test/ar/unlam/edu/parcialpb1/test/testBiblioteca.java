ba136402da663a1615207ba38d4112d155800078
JoRomero1993 <82046186+JoRomero1993@users.noreply.github.com>
2021-06-01 20:11:21 -0300

    vamos a empezar

--- test/ar/unlam/edu/parcialpb1/test/testBiblioteca.java
+++ test/ar/unlam/edu/parcialpb1/test/testBiblioteca.java
@@ -1,5 +1,18 @@
 package ar.unlam.edu.parcialpb1.test;
 
+import org.junit.Test;
+
 public class testBiblioteca {
 
+	/*Se requiere implementar la totalidad de las clases que den solución a 
+	 * la situación planteada 
+y al menos 5 casos pruebas significativos.*/
+@Test
+public void losLibrosExisten() {
+	Biblioteca bibliotecaNacional= new Biblioteca();
+	
+}
+	
+	
+	
 }
